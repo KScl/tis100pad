7af0990a45c73d95438aeb807b5159ef281f5061
Michael Pollind <m_pollen@yahoo.com>
2015-07-24 23:40:27 -0700

    Update run.py
    fixed run problem related to gunicorn

--- src/server/run.py
+++ src/server/run.py
@@ -1,4 +1,5 @@
-from app import app as application
-application.config.from_object('config')
+from app import app
+app.config.from_object('config')
 
-application.run(app.config["IP"],debug=app.config["DEBUG"])
+if __name__ == "__main__":
+ app.run(app.config["IP"],debug=app.config["DEBUG"])
