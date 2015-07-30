fe9640d56989a8cb4ba808032ab51f550a6ab3b4
michael pollind <m_pollen@yahoo.com>
2015-07-29 18:22:58 -0700

    playing with overlay

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('main', ['ngFileUpload', 'ngMessages']);
+
+app.controller("headerController", headerController)
+
+function headerController($scope, Upload, $http, $window, $location) {
+
+
+}
