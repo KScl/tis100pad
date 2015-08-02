e54eb928edef8ca3040a1d7c259e0e6392c1500f
Michael Pollind <mpollind@localhost.localdomain>
2015-08-02 19:39:57 -0400

    adding reCaptcha

--- src/server/app/views/accountService.py
+++ src/server/app/views/accountService.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for, jsonify
+from flask import Blueprint, request, render_template, flash, g, session, redirect, url_for, jsonify,json
 
 from app import db
 from app.model.problem import Problem
+from app import app
+import requests
 
 import string
 import math
@@ -10,11 +12,21 @@ mod = Blueprint('account', __name__, url_prefix='/account')
 
 @mod.route("/login.json")
 def login():
- return "";
+ return ""
 
-@mod.route("/create.json")
+@mod.route("/create.json", methods=['POST'])
 def createAccount():
  name = request.get_json().get("name")
  password = request.get_json().get("password")
  reEnterPassword = request.get_json().get("repassword")
  captcha = request.get_json().get("captcha")
+
+ req = requests.get("https://www.google.com/recaptcha/api/siteverify",{
+ 	"secret": app.config["RECAPTCHA_PRIVATE_TOKEN"],
+    "response":captcha})
+ results = json.loads(req.text)
+ print results
+ if results.success == True:
+  return req.text
+ else:
+  return jsonify(result = "Failed")
