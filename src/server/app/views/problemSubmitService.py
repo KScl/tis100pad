53c794ce5f6e9e056fd9de0931d0da30f4a5523d
michael pollind <m_pollen@yahoo.com>
2015-08-15 15:04:48 -0700

    added way to check identifier

--- src/server/app/views/problemSubmitService.py
+++ src/server/app/views/problemSubmitService.py
@@ -11,3 +11,16 @@ mod = Blueprint('problemPad', __name__, url_prefix='/problemPad')
 @mod.route('/')
 def root():
  return render_template("problemPad.html")
+
+@mod.route('/IdentifierCheck.json',methods=['POST'])
+def IdentityCheck():
+ if Problem.query.filter_by(identifier = request.get_json().get("identifier")).first() == None:
+  return jsonify(result = True)
+ else:
+  return jsonify(result = False)
+
+@mod.route('/submit.json',methods=['POST'])
+def submit():
+ if Problem.query.filter_by(identifier = request.get_json().get("identifier")).first() != None:
+  return jsonify(result = True)
+ return jsonify(result = False)
